6af7b99d6758669e7455097211ccc7fc79ebbeca
karunon <54615644+karunon@users.noreply.github.com>
2025-01-09 17:24:25 +0000

    :sparkles: submit abc170 a

--- contests/abc170/src/bin/a.rs
+++ contests/abc170/src/bin/a.rs
@@ -1,7 +1,24 @@
 fn main() {
     proconio::input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+        _: i32,
     }
 
-    println!("{}", );
+    let ans = if a == 0 {
+        1
+    } else if b == 0 {
+        2
+    } else if c == 0 {
+        3
+    } else if d == 0 {
+        4
+    } else {
+        5
+    };
+
+    println!("{}", ans);
 }
 
