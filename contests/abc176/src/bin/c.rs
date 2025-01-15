8c4e7c6f2651735015fbb8f9397381de2224609b
karunon <54615644+karunon@users.noreply.github.com>
2025-01-15 15:00:23 +0000

    :sparkles: submit abc176 c

--- contests/abc176/src/bin/c.rs
+++ contests/abc176/src/bin/c.rs
@@ -1,7 +1,17 @@
 fn main() {
     proconio::input! {
+        n: usize,
+        v: [i64; n],
     }
 
-    println!("{}", );
+    let mut ans = 0;
+    let mut max_height = 0;
+
+    for i in &v {
+        max_height = max_height.max(*i);
+        ans += max_height - i;
+    }
+
+    println!("{}", ans);
 }
 
