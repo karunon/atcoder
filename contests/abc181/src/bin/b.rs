9d32b333f275dd1d012541db51dd0cf30dda9691
karunon <54615644+karunon@users.noreply.github.com>
2025-01-15 14:44:53 +0000

    :sparkles: submit abc181 b

--- contests/abc181/src/bin/b.rs
+++ contests/abc181/src/bin/b.rs
@@ -1,7 +1,14 @@
 fn main() {
     proconio::input! {
+        n: usize,
+        v: [(i64, i64); n],
     }
 
-    println!("{}", );
+    let mut ans = 0;
+    for (a, b) in &v {
+        ans += (a + b) * (b - a + 1) / 2;
+    }
+
+    println!("{}", ans);
 }
 
