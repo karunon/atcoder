2a27368930801bd7d28f4d70593f1013cf9f13c3
karunon <54615644+karunon@users.noreply.github.com>
2025-01-19 16:33:34 +0000

    :sparkles: submit abc174 b

--- contests/abc174/src/bin/b.rs
+++ contests/abc174/src/bin/b.rs
@@ -1,7 +1,17 @@
 fn main() {
     proconio::input! {
+        n: usize,
+        d: f64,
+        v: [(f64, f64); n],
     }
 
-    println!("{}", );
+    let mut ans = 0;
+    for &(x, y) in &v {
+        if d >= x.hypot(y) {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
 
