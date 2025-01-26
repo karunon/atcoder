a6fa6997a72458bab3db8b925f877c205d4a426b
karunon <54615644+karunon@users.noreply.github.com>
2025-01-26 08:03:32 +0000

    :sparkles: submit abc171 b

---
+++ contests/abc171/src/bin/b.rs
@@ -0,0 +1,17 @@
+fn main() {
+    proconio::input! {
+        n: usize,
+        k: usize,
+        mut p: [i32; n],
+    }
+
+    p.sort();
+
+    let mut ans = 0;
+    for i in 0..k {
+        ans += p[i];
+    }
+
+    println!("{}", ans);
+}
+
