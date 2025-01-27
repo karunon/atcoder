3dd5f3c38d45a6569d66d6b6c9da1e02aafef421
karunon <54615644+karunon@users.noreply.github.com>
2025-01-27 16:29:04 +0000

    :sparkles: submit abc168 a

--- contests/abc168/src/bin/a.rs
+++ contests/abc168/src/bin/a.rs
@@ -1,7 +1,13 @@
 fn main() {
     proconio::input! {
+        n: i32,
     }
 
-    println!("{}", );
+    match n % 10 {
+        2 | 4 | 5 | 7 | 9 => println!("hon"),
+        0 | 1 | 6 | 8 => println!("pon"),
+        3 => println!("bon"),
+        _ => unreachable!(),
+    }
 }
 
